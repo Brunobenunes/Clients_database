f43a807c84bcab93943c28b445ce5c5c7bb291a4
BrunoBenunes <brunobenuness@hotmail.com>
2023-11-13 20:46:38 -0300

    file(query.py) feat(search_by_cpf) Definindo a função que procura no banco de Dados um cliente com o CPF informado

--- noSql/query.py
+++ noSql/query.py
@@ -9,3 +9,22 @@ client = pymongo.MongoClient(
 database = client.clients
 
 clients_list = database.clients_list
+
+def search_by_cpf(cpf_number):
+    ''' Função que procura um determinado cliente com base no cpf
+        params:
+            cpf_number: Numero de Cpf a ser procurado
+        return:
+            Dados do Cliente cujo CPF foi especificado'''
+    find_client = clients_list.find_one({'cpf': cpf_number})
+    if (find_client):
+        return print(f'''
+    Cliente: {find_client['name']}
+    CPF: {cpf_number}
+    Address: {find_client['address']}
+''')
+    return print(f'@@@@ Nenhum Cliente foi encontrado com o CPF: {cpf_number} @@@@')
+
+# Procurando no Algum Cliente com o CPF informado
+search_by_cpf('123.123.123-00')
+
