1bb58d68dacd20775db32791c2e593aae627150f
BrunoBenunes <brunobenuness@hotmail.com>
2023-11-13 21:03:56 -0300

    file(ormSql(main.py)) Criando o Banco de Dados e Criando e Modelando as Tabelas

---
+++ ormSql/main.py
@@ -0,0 +1,54 @@
+'''Criando e recuprando dados em um banco de dados SQL, utilizando ORM'''
+
+from sqlalchemy import (
+    Integer,
+    String,
+    Column,
+    ForeignKey,
+    select,
+    create_engine,
+)
+
+from sqlalchemy.orm import (
+    declarative_base,
+    relationship
+)
+
+Base = declarative_base()
+
+class Client(Base):
+    __tablename__ = 'client'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    cpf = Column(String, unique=True, nullable=False)
+    address = Column(String, nullable=False)
+
+    def __repr__(self):
+        return f'''
+        Cliente: {self.name}
+        ID: {self.id}
+        CPF: {self.cpf}
+        Address: {self.address}'''
+    
+
+class Account(Base):
+    __tablename__ = 'account'
+
+    id = Column(Integer, primary_key=True)
+    type = Column(String, default='Conta-Corrente')
+    branch = Column(String, default='0001')
+    num = Column(String, unique=True, nullable=False)
+    id_client = Column(Integer, ForeignKey('client.id'), unique=True)
+
+    client = relationship(
+        'Client', back_populates='account'
+    )
+
+    def __repr__(self):
+        return f'''
+    Conta: {self.num}
+    ID: {self.id}
+    Client ID: {self.id_client}
+    Type: {self.type}
+    Branch: {self.branch}'''
